6ab09f8c726e61f0e632514b112938e940d3b672
Aleksandra <aaleznova@yandex.ru>
2023-04-11 09:34:20 +0300

    Проектная работа 5. Ревью 1

--- scripts/index.js
+++ scripts/index.js
@@ -1,32 +1,115 @@
-let popup = document.querySelector ('.popup');
-let form = document.querySelector ('.popup__form');
+const getName = document.querySelector ('.profile__name');
+const getDescription = document.querySelector ('.profile__description');
 
-let getName = document.querySelector ('.profile__name');
-let getDescription = document.querySelector ('.profile__description');
+const editButton = document.querySelector ('.profile__edit-button');
+const addButton = document.querySelector ('.profile__add-button');
 
-let nameInput = document.querySelector ('.popup__input_type_name');
-let descriptionInput =  document.querySelector ('.popup__input_type_description');
+const popup = document.querySelectorAll ('.popup');
+const formProfile = document.querySelector ('.popup__form');
 
-let editButton = document.querySelector ('.profile__edit-button');
-let closeButton = document.querySelector ('.popup__close');
+const profilePopup = document.querySelector ('.popup_profile');
+const photoPopup = document.querySelector ('.popup_photo');
+const formPhoto = photoPopup.querySelector ('.popup__form');
 
-function openPopup () {
+const nameInput = document.querySelector ('.popup__input_type_name');
+const descriptionInput =  document.querySelector ('.popup__input_type_description');
+
+const locationInput = document.querySelector ('.popup__input_type_location');
+const linkInput = document.querySelector ('.popup__input_type_link');
+
+
+
+const closeButton = document.querySelectorAll ('.popup__close');
+
+const template = document.querySelector('#template').content;
+const elements = document.querySelector('.elements');
+
+const initialCards = [
+  {
+    name: 'Архыз',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg'
+  },
+  {
+    name: 'Челябинская область',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg'
+  },
+  {
+    name: 'Иваново',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg'
+  },
+  {
+    name: 'Камчатка',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg'
+  },
+  {
+    name: 'Холмогорский район',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg'
+  },
+  {
+    name: 'Байкал',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg'
+  }
+];
+
+function openPopup (popup) {
   popup.classList.add('popup_opened');
+};
+
+function closePopup (popup) {
+  popup.classList.remove('popup_opened');
+};
+
+editButton.addEventListener('click', () => {
   nameInput.value = getName.textContent;
   descriptionInput.value = getDescription.textContent;
-}
+  openPopup(profilePopup);
+});
 
-function closePopup ()  {
-popup.classList.remove('popup_opened');
-}
+formProfile.addEventListener('submit', (evt) => {
+  evt.preventDefault();
+  getName.textContent = nameInput.value;
+  getDescription.textContent = descriptionInput.value;
+  closePopup (profilePopup);
+});
 
-function formHandler (evt) {
-    evt.preventDefault();
-    getName.textContent = nameInput.value;
-    getDescription.textContent = descriptionInput.value;
-    closePopup ();
-}
+addButton.addEventListener('click', () => openPopup(photoPopup));
 
-editButton.addEventListener ('click', openPopup);
-closeButton.addEventListener ('click', closePopup);
-form.addEventListener('submit', formHandler);
+closeButton.forEach((element) => {
+  element.addEventListener('click', () => {
+    closePopup(element.closest('.popup'));
+  });
+});
+
+function createPost(object) {
+  const element = template.querySelector('.element').cloneNode(true);
+  const deleteElement = element.querySelector('.element__delete');
+  const photoElement = element.querySelector('.element__photo');
+  
+  photoElement.alt = object.name;
+  photoElement.src = object.link;
+  element.querySelector('.element__location').textContent = object.name;
+  
+//////////////////////////////////////////////////////////////////////////////////////////////////////
+  const likeElement = element.querySelector('.element__like');
+  likeElement.addEventListener('click', (evt) => evt.target.classList.toggle('element__like_active'));
+//////////////////////////////////////////////////////////////////////////////////////////////////////
+  
+  deleteElement.addEventListener('click', (evt) => evt.target.closest('.element').remove());
+
+  return element;
+};
+
+initialCards.forEach((element) => {
+  const card = createPost(element);
+  elements.append(card);
+});
+
+formPhoto.addEventListener('submit', (evt) => {
+  evt.preventDefault();
+  const object = {name: locationInput.value, link: linkInput.value};
+  const card = createPost(object);
+  elements.prepend(card);
+  closePopup (photoPopup)
+  evt.target.reset();
+});
+
